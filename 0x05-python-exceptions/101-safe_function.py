337e693803fd4101806eafa2e84a8c7e657f37e7
sala2022 <saloua.kasmibakkali@gmail.com>
2023-08-28 21:09:33 +0000

    added files

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as inst:
+        print("Exception: {}".format(inst), file=stderr)
+        return (None)
+    return (result)

780d8920c931af1a0561e5052688b3ad24edc3f2
sala2022 <saloua.kasmibakkali@gmail.com>
2023-10-18 11:34:30 -0700

    add files

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- genre id by show
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
